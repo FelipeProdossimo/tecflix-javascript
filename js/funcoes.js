ef035fa1743d8a650103acf60dc990d6aa743dad
Felipe Prodossimo <felipe.profissional4@gmail.com>
2021-04-01 22:29:59 -0300

    JavaScript alterado

---
+++ js/funcoes.js
@@ -0,0 +1,33 @@
+/*let filme1 = "Soul";
+let filme2 = "A voz do silêncio";
+let filme3 = "Simplesmente acontece";
+*/
+
+//arrays - estilo listas.
+
+let filmes = ["Madagascar", 
+"Rei leão", 
+"Toy story"]
+
+
+/*
+console.log(filme1);
+console.log(filme2);
+console.log(filme3);
+*/
+
+console.log(filmes);
+
+console.log(filmes.length);
+
+
+//acessar o indice
+/*console.log(filmes[0]);
+console.log(filmes[1]);
+console.log(filmes[2]);*/
+
+let quantidadeFilmes = 3;
+while(quantidadeFilmes >= 0){
+    console.log(filmes[quantidadeFilmes]);
+    quantidadeFilmes--;
+} 
